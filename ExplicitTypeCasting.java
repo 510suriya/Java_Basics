344273817f586f06898751d6bb6db083bc5f0446
Suriyapriya V <36623890+510suriya@users.noreply.github.com>
2024-08-31 01:08:56 -0700

    Create ExplicitTypeCasting.java

---
+++ ExplicitTypeCasting.java
@@ -0,0 +1,10 @@
+class ExplicitTypeConversion {
+    public static void main(String[] args) {
+        float f=100.0f;
+        long l=(long)f;
+        int i=(int)f;
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(i);
+    }
+}
