bae16cb62eeb7086154db6f44f8f77eba692f7ab
Suriyapriya V <36623890+510suriya@users.noreply.github.com>
2024-08-31 01:07:14 -0700

    Create ImplicitTypeCasting.java
    Implicit type convesrion

---
+++ ImplicitTypeCasting.java
@@ -0,0 +1,10 @@
+class ImplicitTypeCasting {
+    public static void main(String[] args) {
+        int a=100;
+        long b=a;
+        float c=a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
